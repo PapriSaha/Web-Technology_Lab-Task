bf287b1f06aabdf6f8a3bf32848e6db107ecdaa7
Papri Saha <69990587+PapriSaha@users.noreply.github.com>
2022-06-21 10:58:33 +0600

    Add files via upload

---
+++ Lab task4/phome.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+<body>
+
+ 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Crowdfunding</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Registration.php">Registration</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="phome.php">Logout</a>
+      </li>
+       
+    </form>
+  </div>
+</nav>
+
+
+<?php
+echo "<p>Copyright &copy; 1999-" . date("Y");
+?>
+
+</body>
+</html>
